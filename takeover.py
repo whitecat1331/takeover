ffe6dfe552fa27f543c7b9692834d612ce583612
edoardottt <edoardott@gmail.com>
2021-03-22 19:44:11 +0100

    update

--- takeover.py
+++ takeover.py
@@ -50,17 +50,19 @@ def PERCENT(x, y):
 services = {
     "AWS/S3": {"error": r"The specified bucket does not exist"},
     "BitBucket": {"error": r"Repository not found"},
-    "Github": {"error": r"There isn\\\'t a Github Pages site here\."},
+    "Github": {
+        "error": r"There isn\\\'t a Github Pages site here\.|a Github Pages site here"
+    },
     "Shopify": {"error": r"Sorry\, this shop is currently unavailable\."},
     "Fastly": {"error": r"Fastly error\: unknown domain\:"},
     "Ghost": {
         "error": r"The thing you were looking for is no longer here\, or never was"
     },
     "Heroku": {
-        "error": r"no-such-app.html|<title>no such app</title>|herokucdn.com/error-pages/no-such-app.html"
+        "error": r"no-such-app.html|<title>no such app</title>|herokucdn.com/error-pages/no-such-app.html|No such app"
     },
     "Pantheon": {
-        "error": r"The gods are wise, but do not know of the site which you seek."
+        "error": r"The gods are wise, but do not know of the site which you seek|404 error unknown site"
     },
     "Tumbler": {
         "error": r"Whatever you were looking for doesn\\\'t currently exist at this address."
@@ -69,14 +71,15 @@ services = {
     "TeamWork": {"error": r"Oops - We didn\'t find your site."},
     "Helpjuice": {"error": r"We could not find what you\'re looking for."},
     "Helpscout": {"error": r"No settings were found for this company:"},
-    "Cargo": {"error": r"<title>404 &mdash; File not found</title>"},
-    "Uservoice": {"error": r"This UserVoice subdomain is currently available!"},
-    "Surge": {"error": r"project not found"},
+    "Cargo": {"error": r"<title>404 &mdash; File not found</title>|404 Not Found"},
+    "Uservoice": {"error": r"This UserVoice subdomain is currently available"},
+    "Surge.sh": {"error": r"project not found"},
     "Intercom": {
         "error": r"This page is reserved for artistic dogs\.|Uh oh\. That page doesn\'t exist</h1>"
     },
     "Webflow": {
-        "error": r"<p class=\"description\">The page you are looking for doesn\'t exist or has been moved.</p>"
+        "error": r"<p class=\"description\">The page you are looking for doesn\'t exist or has been \
+moved.</p>|The page you are looking for doesn\'t exist or has been moved"
     },
     "Kajabi": {"error": r"<h1>The page you were looking for doesn\'t exist.</h1>"},
     "Thinkific": {
@@ -95,11 +98,11 @@ services = {
     "Bigcartel": {"error": r"<h1>Oops! We couldn&#8217;t find that page.</h1>"},
     "ActiveCampaign": {"error": r"alt=\"LIGHTTPD - fly light.\""},
     "Campaignmonitor": {
-        "error": r"Double check the URL or <a href=\"mailto:help@createsend.com"
+        "error": r"Double check the URL or <a href=\"mailto:help@createsend.com|Trying to access your account"
     },
     "Acquia": {
         "error": r"The site you are looking for could not be found.|If you are an Acquia Cloud \
-customer and expect to see your site at this address"
+customer and expect to see your site at this address|Web Site Not Found"
     },
     "Proposify": {
         "error": r"If you need immediate assistance, please contact <a href=\"mailto:support@proposify.biz"
@@ -113,16 +116,40 @@ customer and expect to see your site at this address"
     "Vend": {"error": r"Looks like you\'ve traveled too far into cyberspace."},
     "Jetbrains": {"error": r"is not a registered InCloud YouTrack."},
     "Smartling": {"error": r"Domain is not configured"},
-    "Pingdom": {"error": r"pingdom"},
-    "Tilda": {"error": r"Domain has been assigned"},
+    "Pingdom": {"error": r"pingdom|Sorry, couldn\'t find the status page"},
+    "Tilda": {"error": r"Domain has been assigned|Please renew your subscription"},
     "Surveygizmo": {"error": r"data-html-name"},
-    "Mashery": {"error": r"Unrecognized domain <strong>"},
+    "Mashery": {"error": r"Unrecognized domain <strong>|Unrecognized domain"},
     "Divio": {"error": r"Application not responding"},
     "feedpress": {"error": r"The feed has not been found."},
-    "readme": {"error": r"Project doesnt exist... yet!"},
+    "Readme.io": {"error": r"Project doesnt exist... yet!"},
     "statuspage": {"error": r"You are being <a href=\'https>"},
     "zendesk": {"error": r"Help Center Closed"},
     "worksites.net": {"error": r"Hello! Sorry, but the webs>"},
+    "Agile CRM": {"error": r"this page is no longer available"},
+    "Anima": {
+        "error": r"try refreshing in a minute|this is your website and you've just created it"
+    },
+    "Fly.io": {"error": r"404 Not Found"},
+    "Gemfury": {"error": r"This page could not be found"},
+    "HatenaBlog": {"error": r"404 Blog is not found"},
+    "Kinsta": {"error": r"No Site For Domain"},
+    "LaunchRock": {
+        "error": r"It looks like you may have taken a wrong turn somewhere|worry...it happens to all of us"
+    },
+    "Ngrok": {"error": r"ngrok.io not found"},
+    "SmartJobBoard": {
+        "error": r"This job board website is either expired or its domain name is invalid"
+    },
+    "Strikingly": {"error": r"page not found"},
+    "Tumblr": {
+        "error": r"Whatever you were looking for doesn\'t currently exist at this address"
+    },
+    "Uberflip": {
+        "error": r"hub domain\, The URL you\'ve accessed does not provide a hub"
+    },
+    "Unbounce": {"error": r"The requested URL was not found on this server"},
+    "Uptimerobot": {"error": r"page not found"},
 }
 
 
@@ -227,7 +254,7 @@ def checkpath(path):
 
 
 def readfile(path):
-    info('Read wordlist.. "%s"' % path)
+    info('Read wordlist... "%s"' % path)
     return [x.strip() for x in open(checkpath(path), "r")]
 
 
